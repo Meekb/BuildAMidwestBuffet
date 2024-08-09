9f9ad57792f5e8c439621389e402fe4fcc4d5424
Beth <bethm.meeker@gmail.com>
2024-08-09 14:08:42 -0500

    Change starter class file to BuffetSide and edit tests

--- spec/buffet_dessert_spec.rb
+++ spec/buffet_dessert_spec.rb
@@ -4,11 +4,18 @@ require './lib/buffet_dessert'
 RSpec.describe BuffetDessert do
   it 'has a name' do
     dessert = BuffetDessert.new('Apple Pie')
+
     expect(dessert.name).to eq('Apple Pie')
   end
 
   it 'can be a classic Midwest dessert' do
-    dessert = BuffetDessert.new('Banana Pudding', classic_midwest: true)
-    expect(dessert.classic_midwest?).to be true
+    dessert_1 = BuffetDessert.new('Banana Pudding', classic_midwest: true)
+    dessert_2 = BuffetDessert.new('Assorted Cookies')
+
+    expect(dessert_1.name).to eq('Banana Pudding')
+    expect(dessert_1.classic_midwest?).to be true
+
+    expect(dessert_2.name).to eq('Assorted Cookies')
+    expect(dessert_2.classic_midwest?).to be false
   end
 end
--- spec/buffet_side_spec.rb
+++ spec/buffet_side_spec.rb
@@ -3,13 +3,18 @@ require './lib/buffet_side'
 
 RSpec.describe BuffetSide do
   it 'has a name' do
-    side = BuffetSide.new()
+    side = BuffetSide.new('Mashed Potatoes')
     expect(side.name).to eq('Mashed Potatoes')
   end
 
-  it 'can be labeled as a comfort food' do
-    side = BuffetSide.new('Sweet Corn Pudding', comfort_food: true)
-    expect(side.name).to eq('Sweet Corn Pudding')
-    expect(side.comfort_food?).to be true
+  xit 'can be labeled as a comfort food' do
+    side_1 = BuffetSide.new('Southern Baked Mac & Cheese', comfort_food: true)
+    side_2 = BuffetSide.new('Fried Ravioli')
+
+    expect(side_1.name).to eq('Sweet Corn Pudding')
+    expect(side_1.comfort_food?).to be true
+
+    expect(side_2.name).to eq('Fried Ravioli')
+    expect(side_2.comfort_food?).to be false
   end
 end
