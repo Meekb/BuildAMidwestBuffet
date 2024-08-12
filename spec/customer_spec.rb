d6fc2ce34f82a8b5b497fe92708349eb7035ac84
Beth <bethm.meeker@gmail.com>
2024-08-12 06:49:11 -0500

    Refactor some customer tests for drown plate in Ranch functionality

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe Customer do
     expect(customer.name).to eq('Mike')
   end
 
-  xit 'has a plate' do
+  it 'has a plate' do
     customer = Customer.new('Barb')
     expect(customer.plate).to be_a(Hash)
   end
 
-  xit 'has a plate with dishes, sides, and desserts' do
+  it 'has a plate with dishes, sides, and desserts' do
     customer = Customer.new('Merle')
     expect(customer.plate[:dishes]).to be_an(Array)
     expect(customer.plate[:sides]).to be_an(Array)
     expect(customer.plate[:desserts]).to be_an(Array)
   end
 
-  xit 'can fill their plate with dishes, sides, and desserts' do
+  it 'can fill their plate with dishes, sides, and desserts' do
     customer = Customer.new('Scooter')
 
     customer.add_dish(BuffetDish.new('Chicken Fingers', portion_size: 'Mound'))
@@ -34,23 +34,29 @@ RSpec.describe Customer do
     expect(customer.plate[:desserts].count).to eq(1)
  end
 
-  xit 'knows if the plate has been drowned in Ranch' do
+  it 'knows if the plate has been drowned in Ranch' do
     customer = Customer.new('Barb')
     expect(customer.plate_drowned?).to be false
   end
 
-  xit 'can say "Ohp! Let me scootch by ya for the Ranch..."' do
+  it 'can drown the plate in Ranch' do
+    customer = Customer.new('Barb')
+    customer.drown_plate_in_ranch
+    expect(customer.plate_drowned?).to be true
+  end
+
+  it 'can say "Ohp! Let me scootch by ya for the Ranch..."' do
     customer = Customer.new('Barb')
     expect(customer.say_ohp).to eq('Ohp! Let me scootch by ya for the Ranch...')
   end
 
-  xit 'drowns plate in Ranch if it says "Ohp!"' do
+  it 'drowns plate in Ranch if it says "Ohp!"' do
     customer = Customer.new('Barb')
     expect(customer.say_ohp).to eq('Ohp! Let me scootch by ya for the Ranch...')
     expect(customer.plate_drowned?).to be true
   end
 
-  xit 'tracks the number of times it says \'Ohp\'' do
+  it 'tracks the number of times it says \'Ohp\'' do
     customer = Customer.new('Barb')
     customer.say_ohp
     expect(customer.ohp_count).to eq(1)
