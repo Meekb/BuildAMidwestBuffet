e22d7f4a9d4c09fcde7b40c9368325298db0b7cf
Beth <bethm.meeker@gmail.com>
2024-08-12 07:48:49 -0500

    Add tests for count for BuffetDish and ability to refill_dish. Add to x_solutions.

--- x_solutions/buffet_dish.rb
+++ x_solutions/buffet_dish.rb
@@ -1,12 +1,22 @@
 class BuffetDish
     attr_reader :name,
                 :type,
-                :portion_size
+                :portion_size,
+                :count
 
     def initialize(name, portion_size: 'Heaping')
         @name = name
         @type = 'main'
         @portion_size = portion_size
+        @count = 25
+    end
+
+    def decrease_count
+        @count -= 5
+    end
+
+    def refill_dish
+        @count += 25
     end
 
     def inspect
