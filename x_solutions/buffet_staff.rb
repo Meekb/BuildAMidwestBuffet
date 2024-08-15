80113c91459af0965dad39d29071757274107850
Beth <bethm.meeker@gmail.com>
2024-08-15 17:09:53 -0500

    Update x-solutions

--- x_solutions/buffet_staff.rb
+++ x_solutions/buffet_staff.rb
@@ -7,21 +7,44 @@ class BuffetStaff
     @hustle_meter = 10
   end
 
+  def check_hustle
+    @hustle_meter
+  end
+
+  def decrease_hustle
+    @hustle_meter -= 1
+  end
+
   def refill_buffet(buffet)
     puts "Refilling the buffet!"
-    buffet[:dishes].each do |dish|
-        next unless dish.count <= 0
+    buffet.dishes.each do |dish|
+      if dish.count <= 0
         dish.refill_dish
+        decrease_hustle
+        puts "Decreased hustle after refilling dish: #{@hustle_meter}"
+      else
+        puts "Dish count is not <= 0; hustle_meter remains at #{@hustle_meter}"
+      end
     end
-
-    buffet[:sides].each do |side|
-        next unless side.count <= 0
+  
+    buffet.sides.each do |side|
+      if side.count <= 0
         side.refill_side
+        decrease_hustle
+        puts "Decreased hustle after refilling side: #{@hustle_meter}"
+      else
+        puts "Side count is not <= 0; hustle_meter remains at #{@hustle_meter}"
+      end
     end
-
-    buffet[:desserts].each do |dessert|
-        next unless dessert.count <= 0
+  
+    buffet.desserts.each do |dessert|
+      if dessert.count <= 0
         dessert.refill_dessert
+        decrease_hustle
+        puts "Decreased hustle after refilling dessert: #{@hustle_meter}"
+      else
+        puts "Dessert count is not <= 0; hustle_meter remains at #{@hustle_meter}"
+      end
     end
   end
 
